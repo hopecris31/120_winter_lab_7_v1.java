0a254c54f5bf367c1649b284a3216b9880cc2e62
hopecris31 <hope.crisafi@gmail.com>
2022-02-17 16:24:55 -0500

    Initial commit

---
+++ src/Die.java
@@ -0,0 +1,32 @@
+import  java.util.Random;
+
+public class Die {
+
+    public int currSide; //should this be private or public
+    private int numSides;
+    public final int DEFAULT_SIDES = 6;
+
+
+    /**
+     * constructor, initializes the die
+     */
+    public Die(int sides) { //do not have to define data type in constructor
+        numSides = sides;
+        currSide = sides;
+    }
+
+    /**
+     * roll the dice, select random number 1-n.
+     * set current side equal to roll value
+     */
+    public void roll() { //void means does not return anything
+        Random rollNum; //declaring that rollNum will be of the random class when written
+        rollNum = new Random(); //the roll is a random int
+        currSide = rollNum.nextInt(numSides) +1; //variable.method(parameter) // returns a random number in rollNum range
+    }
+
+    public int getValue() {
+        return currSide;
+    }
+}
+
