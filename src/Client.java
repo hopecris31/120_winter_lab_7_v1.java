fb013f96c593bff6673a5e9070dba9712d06a804
hopecris31 <hope.crisafi@gmail.com>
2022-02-17 20:46:40 -0500

    ugh

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,5 @@
+
+
 public class Client {
     public static void main(String[] args) {
 
@@ -11,11 +13,11 @@ public class Client {
         die2 = new Die(TWELVE_SIDES);
 
         while (die1.getValue() != die2.getValue() && die2.getValue() != die1.getValue()*2){
-            dice1.roll();
-            dice2.roll();
-            System.out.println("you rolled:", dice1.get_value(), "and", dice2.get_value());
-            if (dice1.get_value() == dice2.get_value()*2 || dice2.get_value() == dice1.get_value()*2:){
-                System.out.println("game over, double value!")
+            die1.roll();
+            die2.roll();
+            System.out.println("you rolled:" + die1.getValue() + "and" + die2.getValue());
+            if (die1.getValue() == die2.getValue()*2 || die2.getValue() == die1.getValue()*2){
+                System.out.println("game over, double value!");
             }
 
 
